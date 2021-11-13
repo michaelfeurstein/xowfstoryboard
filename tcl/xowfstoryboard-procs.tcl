6cdd858d3312df9d9dd8b3c02c70511f552134c3
Michael Feurstein <michael.feurstein@wu.ac.at>
2021-11-14 00:34:58 +0100

    initial commit

---
+++ tcl/xowfstoryboard-procs.tcl
@@ -0,0 +1,15 @@
+::xo::db::require package xowf
+
+namespace eval ::xowfstoryboard {
+
+  ::xo::PackageMgr create ::xowfstoryboard::Package \
+      -package_key "xowfstoryboard" \
+	  -pretty_name "Storyboard Editor" \
+      -superclass ::xowf::Package
+
+  ::xowfstoryboard::Package instproc initialize {} {
+        ns_log notice "++++ CALL ::xowfstoryboard::initialize"
+        next
+  }
+
+}
