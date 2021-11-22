2bc0c11d9f3a0625a7ffa1a7acf2ec65966adbdc
Michael Feurstein <michael.feurstein@wu.ac.at>
2021-11-22 15:07:36 +0100

    integration question

--- tcl/form-field-procs.tcl
+++ tcl/form-field-procs.tcl
@@ -1,10 +1,32 @@
+source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/language_model.tcl
+source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/parser.tcl
+source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/worker.tcl
+source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/expression_builder.tcl
+
+namespace import StoryBoard::*
+
 namespace eval ::xowiki::formfield {
 
-  Class create monaco_storyboard -superclass monaco_html_sandbox -ad_doc {
+  Class create monaco_storyboard -superclass monaco -ad_doc {
     trying to extend on what antonio did
   } -parameter {
-    {preview_window true}
-    {preview_inline true}
+    {storyboardSyntax key-value}
+  }
+
+  monaco_storyboard instproc pretty_value args {
+	StoryBoard::Video new -id video1
+    ns_log notice "--- monaco_storyboard videos:[llength [StoryBoard::Video info instances -closure]]"
+	ns_log notice "--- monaco_storyboard value:[:value]"
+	set storyboard [:fromBase64 [:value]]
+	ns_log notice "--- monaco_storyboard sb:$storyboard"
+	set internalParser [StoryboardParser new -storyboard $storyboard]
+	set internalBuilder [StoryBoard::StoryboardBuilder new]
+	set module [$internalBuilder from [$internalParser storyboardDict get]]
+	ns_log notice "\nModule: [llength [StoryBoard::Module info instances -closure]]"
+	ns_log notice " - id: [[StoryBoard::Module info instances -closure] id get]"
+	ns_log notice " - title: [[StoryBoard::Module info instances -closure] title get]"
+	ns_log notice " - structure: [[StoryBoard::Module info instances -closure] structure get]"
+	next
   }
 
 }
