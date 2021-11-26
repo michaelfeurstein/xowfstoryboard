c11e69c0f3a3af115be43aec43dd0b25ba5e7e96
Michael Feurstein <michael.feurstein@wu.ac.at>
2021-11-26 16:22:37 +0100

    render preview and status in view page
    when viewing a storyboard:
    - show meta info below read only editor
    - show html preview next to read only editor

--- tcl/form-field-procs.tcl
+++ tcl/form-field-procs.tcl
@@ -1,7 +1,10 @@
 source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/language_model.tcl
 source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/parser.tcl
+source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/visitor.tcl
 source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/worker.tcl
 source /var/www/oacs-5-10-0/packages/xowfstoryboard/tcl/storyboard-language/expression_builder.tcl
+
+package req tdom
 
 namespace eval ::xowiki::formfield {
 
@@ -12,49 +15,78 @@ namespace eval ::xowiki::formfield {
   }
 
   monaco_storyboard instproc pretty_value args {
-	namespace path ::StoryBoard
-	StoryBoard::Video new -id video1
-    ns_log notice "--- monaco_storyboard videos:[llength [StoryBoard::Video info instances -closure]]"
-	ns_log notice "--- monaco_storyboard value:[:value]"
+	#
+	# Variables
+	#
+
 	set storyboard [:fromBase64 [:value]]
+	set base64 [:value]
+
+	#
+	# StoryBoard preparations
+	#
+
+	namespace path ::StoryBoard
 	ns_log notice "--- monaco_storyboard sb:$storyboard"
 	set internalParser [StoryboardParser new -storyboard $storyboard]
 	set internalBuilder [StoryBoard::StoryboardBuilder new]
 	set module [$internalBuilder from [$internalParser storyboardDict get]]
-	ns_log notice "\nModule: [llength [StoryBoard::Module info instances -closure]]"
-	ns_log notice " - id: [[StoryBoard::Module info instances -closure] id get]"
-	ns_log notice " - title: [[StoryBoard::Module info instances -closure] title get]"
-	ns_log notice " - structure: [[StoryBoard::Module info instances -closure] structure get]"
-	set modules [llength [StoryBoard::Module info instances -closure]]
+
+	set visitor [HTMLVisitor new]
+	set htmlResult [$visitor evaluate $module]
+
+	set sb_modules [llength [StoryBoard::Module info instances -closure]]
+	set sb_id [[StoryBoard::Module info instances -closure] id get]
+	set sb_title [[StoryBoard::Module info instances -closure] title get]
+	set sb_structure [[StoryBoard::Module info instances -closure] structure get]
+
+	set htmlPreview [$htmlResult asHTML]
+	#set htmlPreview "something"
+
+	#
+	# JS preparations
+	#
 
 	template::add_body_script -script {var require = { paths: { 'vs': '/resources/xowf-monaco-plugin/monaco-editor/min/vs' } };}
 	template::add_body_script -src urn:ad:js:monaco:min/vs/loader
 	template::add_body_script -src urn:ad:js:monaco:min/vs/editor/editor.main.nls
 	template::add_body_script -src urn:ad:js:monaco:min/vs/editor/editor.main
-
-	set base64 [:value]
 	template::add_body_handler -event load -script [subst -nocommands {
 
-	var srcDoc = document.getElementById('${:id}-srcdoc');
-	var page = xowf.monaco.b64_to_utf8(srcDoc.innerHTML);
-	console.log("page: " + page);
+		var srcDoc = document.getElementById('${:id}-srcdoc');
+		var page = xowf.monaco.b64_to_utf8(srcDoc.innerHTML);
+		console.log("page: " + page);
 
-	xowf.monaco.editors.push(monaco.editor.create(document.getElementById('${:id}'), {
-          language: '${:language}', minimap: {enabled: ${:minimap}}, readOnly: true, theme: '${:theme}'
-      }));
-	xowf.monaco.editors[xowf.monaco.editors.length-1].setValue(xowf.monaco.b64_to_utf8('$base64'));
-
+		xowf.monaco.editors.push(monaco.editor.create(document.getElementById('${:id}'), {
+		  language: '${:language}', minimap: {enabled: ${:minimap}}, readOnly: true, theme: '${:theme}'
+		}));
+		xowf.monaco.editors[xowf.monaco.editors.length-1].setValue(xowf.monaco.b64_to_utf8('$base64'));
 
 	}]
 
-	# CONTINUE HERE: inside the div class Form-monaco setup a structure for a side by side view of editor and generated preview. Step A: this 2 column structzre and Step B: start visitor to generate preview for the right side
+	#
+	# Return HTML preparations
+	#
+
+	# CONTINUE HERE: inside the div class Form-monaco setup a structure for a side by side view of editor and generated preview. Step B: start visitor to generate preview for the right side
 	return [subst -nocommands {
 
 	 <template id="${:id}-srcdoc" style="display:none;">$base64</template>
 
-	 <div>Modules: $modules</div>
+	 <div class="row">
+		<div id="${:id}" class="${:CSSclass} col-md-8" style="width: ${:width}; height: ${:height};"></div>
+		<div class="col-md-4">$htmlPreview</div>
+	 </div>
+	 <div style="width: ${:width}; height: ${:height};" id="${:id}-status">
+		Modules: $sb_modules
+		<br>
+		ID: $sb_id
+		<br>
+		Title: $sb_title
+		<br>
+		Structure: $sb_structure
+	 </div>
 
-	 <div id="${:id}" classe="${:CSSclass}" style="width: ${:width}; height: ${:height};"></div>
 
 
 	}]
